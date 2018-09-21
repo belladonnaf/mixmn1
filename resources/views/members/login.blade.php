32c57f31e39d2e05ace1f298fb4f606d5ed13117
belladonnaf <belladonnaf@gmail.com>
2018-09-21 19:19:33 +0900

    Change Music icon

--- resources/views/members/login.blade.php
+++ resources/views/members/login.blade.php
@@ -41,7 +41,7 @@
                     <div class="block-content block-content-full px-lg-5 py-md-5 py-lg-6">
                         <div class="media">
                             <a class="img-link mr-3" href="javascript:void(0)">
-                                <img class="img-avatar img-avatar-thumb" src="assets/media/avatars/avatar13.jpg" alt="">
+                                <img class="img-avatar img-avatar-thumb" src="/media/images/music.png" alt="">
                             </a>
                             <div class="media-body">
                                 <p class="text-white font-w600 mb-1">
