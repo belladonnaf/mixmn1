5c5a8e9776e86b4dcbd23543e013d5fdb0c08886
belladonnaf <belladonnaf@gmail.com>
2018-09-21 18:54:46 +0900

    Add Testing..

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,8 @@
 // Example Routes
 use Illuminate\Support\Facades\DB;
 
-$site_status = DB::select('select * from site_status ');
+$site_status = DB::select('select * from site_status limit 0,1');
+var_dump($site_status);
   
 Route::view('/', 'members/login',['site_status' => $site_status]);
 
