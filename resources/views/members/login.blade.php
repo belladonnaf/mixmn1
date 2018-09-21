193e01500a1784ed26ed53238ddd5c57f6fce3cd
belladonnaf <belladonnaf@gmail.com>
2018-09-21 19:22:41 +0900

    Change media path

--- resources/views/members/login.blade.php
+++ resources/views/members/login.blade.php
@@ -5,7 +5,7 @@
 <div class="row no-gutters justify-content-center bg-body-dark">
     <div class="hero-static col-sm-10 col-md-8 col-xl-6 d-flex align-items-center p-2 px-sm-0">
         <!-- Sign In Block -->
-        <div class="block block-rounded block-transparent block-fx-pop w-100 mb-0 overflow-hidden bg-image" style="background-image: url('assets/media/photos/photo20@2x.jpg');">
+        <div class="block block-rounded block-transparent block-fx-pop w-100 mb-0 overflow-hidden bg-image" style="background-image: url('media/photos/photo20@2x.jpg');">
             <div class="row no-gutters">
                 <div class="col-md-6 order-md-1 bg-white">
                     <div class="block-content block-content-full px-lg-5 py-md-5 py-lg-6">
@@ -41,7 +41,7 @@
                     <div class="block-content block-content-full px-lg-5 py-md-5 py-lg-6">
                         <div class="media">
                             <a class="img-link mr-3" href="javascript:void(0)">
-                                <img class="img-avatar img-avatar-thumb" src="/media/images/music.png" alt="">
+                                <img class="img-avatar img-avatar-thumb" src="/media/images/music-icon.png" alt="">
                             </a>
                             <div class="media-body">
                                 <p class="text-white font-w600 mb-1">
