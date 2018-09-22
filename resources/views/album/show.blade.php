9182d6b38f119417eb31cda90fd550add322c5f7
belladonnaf <belladonnaf@gmail.com>
2018-09-23 01:58:59 +0900

    another UI change

--- resources/views/album/show.blade.php
+++ resources/views/album/show.blade.php
@@ -6,19 +6,6 @@
 
 @section('content')
     <!-- Hero -->
-<div class="bg-body-light">
-    <div class="content content-full">
-        <div class="d-flex flex-column flex-sm-row justify-content-sm-between align-items-sm-center">
-            <h1 class="flex-sm-fill font-size-h2 font-w400 mt-2 mb-0 mb-sm-2">Archives _ Group Index</h1>
-            <nav class="flex-sm-00-auto ml-sm-3" aria-label="breadcrumb">
-                <ol class="breadcrumb">
-                    <li class="breadcrumb-item">Tables</li>
-                    <li class="breadcrumb-item active" aria-current="page">Group Index</li>
-                </ol>
-            </nav>
-        </div>
-   </div>
-</div>
 <!-- END Hero -->
 
 <!-- Page Content -->
