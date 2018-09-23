affcd81b8f69eac2f072867d331972a7bb72f645
belladonnaf <belladonnaf@gmail.com>
2018-09-24 01:14:07 +0900

    some fixed ..

--- app/Http/Controllers/ArchivesController.php
+++ app/Http/Controllers/ArchivesController.php
@@ -6,8 +6,10 @@ use Illuminate\Http\Request;
 use App\User;
 use Illuminate\Support\Facades\DB;
 
-if ( !Request()->session()->get("login_id") ){
-	return redirect('/');		
+if ( !Request()->hasSession() && !Request()->session()->get("login_id")){
+
+		return redirect('/');		
+
 }
 
 class ArchivesController extends Controller
