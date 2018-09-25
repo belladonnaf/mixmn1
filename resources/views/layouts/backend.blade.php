6d426ea0dbc636b57f745b908f80c1ef27f37d16
belladonnaf <belladonnaf@gmail.com>
2018-09-25 18:45:44 +0900

    testing

--- resources/views/layouts/backend.blade.php
+++ resources/views/layouts/backend.blade.php
@@ -245,6 +245,7 @@ $cnt_value = count($db_value);
 <?php 
 $sql = " select count(*) cnt from mp3_favorite where user_id = ? and album_id > 0 ";
 var_dump(request()->session()->get('login_id'));
+exit;
 $fav_cnt=DB::select($sql,request()->session()->get('login_id'))[0]->cnt;
 
 ?>
