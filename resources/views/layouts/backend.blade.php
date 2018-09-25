c1106c0402977dd497fe15e2cb0c3abed4fb9f32
belladonnaf <belladonnaf@gmail.com>
2018-09-26 00:09:04 +0900

    fixed.

--- resources/views/layouts/backend.blade.php
+++ resources/views/layouts/backend.blade.php
@@ -282,7 +282,7 @@ $stream_set_cnt=DB::select($sql,[(int)$login_id])[0]->cnt;
                         <div class="dropdown d-inline-block">
                             <button type="button" class="btn btn-dual" id="page-header-notifications-dropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                 <i class="fa fa-fw fa-bell"></i>
-                                <span class="badge badge-secondary badge-pill">1</span>
+                                <span class="badge badge-secondary badge-pill">5</span>
                             </button>
                             <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right p-0" aria-labelledby="page-header-notifications-dropdown">
                                 <div class="bg-primary-darker rounded-top font-w600 text-white text-center p-3">
@@ -300,7 +300,50 @@ $stream_set_cnt=DB::select($sql,[(int)$login_id])[0]->cnt;
                                             </div>
                                         </a>
                                     </li>
-
+                                    <li>
+                                        <a class="text-dark media py-2" href="javascript:void(0)">
+                                            <div class="mx-3">
+                                                <i class="fa fa-fw fa-user-plus text-info"></i>
+                                            </div>
+                                            <div class="media-body font-size-sm pr-2">
+                                                <div class="font-w600">New Subscriber was added! You now have 2580!</div>
+                                                <div class="text-muted font-italic">10 min ago</div>
+                                            </div>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a class="text-dark media py-2" href="javascript:void(0)">
+                                            <div class="mx-3">
+                                                <i class="fa fa-fw fa-times-circle text-danger"></i>
+                                            </div>
+                                            <div class="media-body font-size-sm pr-2">
+                                                <div class="font-w600">Server backup failed to complete!</div>
+                                                <div class="text-muted font-italic">30 min ago</div>
+                                            </div>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a class="text-dark media py-2" href="javascript:void(0)">
+                                            <div class="mx-3">
+                                                <i class="fa fa-fw fa-exclamation-circle text-warning"></i>
+                                            </div>
+                                            <div class="media-body font-size-sm pr-2">
+                                                <div class="font-w600">You are running out of space. Please consider upgrading your plan.</div>
+                                                <div class="text-muted font-italic">1 hour ago</div>
+                                            </div>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a class="text-dark media py-2" href="javascript:void(0)">
+                                            <div class="mx-3">
+                                                <i class="fa fa-fw fa-plus-circle text-primary"></i>
+                                            </div>
+                                            <div class="media-body font-size-sm pr-2">
+                                                <div class="font-w600">New Sale! + $30</div>
+                                                <div class="text-muted font-italic">2 hours ago</div>
+                                            </div>
+                                        </a>
+                                    </li>
                                 </ul>
                             </div>
                         </div>
