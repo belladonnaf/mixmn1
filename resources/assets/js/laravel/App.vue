9895fce1bb698a835cd51db80a02369bef1571a8
belladonnaf <belladonnaf@gmail.com>
2018-09-25 10:15:24 +0900

    revert

--- resources/assets/js/laravel/App.vue
+++ resources/assets/js/laravel/App.vue
@@ -1,22 +1,49 @@
 <template>
   <div id="app">
-    <aplayer :audio="audio" :lrcType="1" />
+    <aplayer :audio="audio" :lrcType="1"></aplayer>
   </div>
 </template>
 
 <script>
+
 export default {
   name: 'App',
   data() {
+	
     return {
-      audio: {
-        name: 'I Really Like You à¤Úìä³?÷ú£¨Cover Carly Rae Jepsen£©',
-        artist: 'ê¨í­',
-        url: 'http://pdacsgxq7.bkt.clouddn.com/mp3/mucho.mp3',
-        cover: 'https://p1.music.126.net/UhbkP71d9KTMsdNczCf2wA==/109951162986700564.jpg?param=300y300',
-        lrc: '[by:]\n[00:00.00]  Jepsen\n[00:01.00] : Angie\n[00:07.07]Ya no puedo parar\n[00:08.68]Necesito ahora saberlo\n[00:10.94]Creo que puedo volar\n[00:12.57]Contigo hasta el cielo\n[00:14.96]Asi que mirame\n[00:16.46]He esperado tanto esto\n[00:18.89]Siento que morire\n[00:20.30]En este segundo\n[00:23.85]Por la noche entretenidos\n[00:27.81]No se como ya decirlo\n[00:31.49]Esto es muy pronto, Y se que no es amor\n[00:37.98]\"Pero tengo que decirte\"\n[00:39.82]En serio tu me gustas\n[00:41.58]mucho mucho mucho mucho\n[00:44.28]Yo te quiero Si me quieres Ahora dimelo\n[00:47.74]En serio tu me gustas\n[00:49.60]mucho mucho mucho mucho\n[00:52.26]Yo te quiero Si me quieres Ahora dimelo\n[00:55.34]Dime hable de mas\n[00:58.47]No puedo pensar\n[01:00.63]En mi mente estas\n[01:03.50]En serio tu me gustas\n[01:05.18]mucho mucho mucho mucho\n[01:08.00]Yo te quiero Si me quieres Ahora dimelo\n[01:11.94]Todo lo que dices\n[01:13.62]En mi corazon lo tengo\n[01:16.00]Yo quiero estar\n[01:17.49]Siempre en tu mente ya\n[01:19.95]Quiero que tu y yo\n[01:21.41]Estemos solo un momento\n[01:23.77]Bebiendo tus labios\n[01:25.28]Me mente caera\n[01:28.80]Por la noche entretenidos\n[01:32.73]No se como ya decirlo\n[01:36.50]Esto es muy pronto, Y se que no es amor\n[01:42.86]\"Pero tengo que decirte\"\n[01:44.80]En serio tu me gustas\n[01:46.47]mucho mucho mucho mucho\n[01:49.21]Yo te quiero Si me quieres Ahora dimelo\n[01:52.70]En serio tu me gustas\n[01:54.36]mucho mucho mucho mucho\n[01:57.15]Yo te quiero Si me quieres Ahora dimelo\n[02:00.44]Dime hable de mas\n[02:03.46]No puedo pensar\n[02:05.63]En mi mente estas\n[02:08.49]En serio tu me gustas\n[02:10.03]mucho mucho mucho mucho\n[02:12.84]Yo te quiero Si me quieres Ahora dimelo\n[02:16.28]De donde sacaste tus bellos ojos\n[02:20.22]No se como actuar deberia dejarlo\n[02:24.25]El tiempo acabara\n[02:26.37]Loca debo estar\n[02:28.21]\"Tengo algo que decirte\"\n[02:29.31]\"Tengo algo que decirte\"\n[02:30.37]Estoy embarazada\n[02:31.54]Es broma\n[02:33.74]En serio tu me gustas\n[02:35.23]mucho mucho mucho mucho\n[02:38.11]Yo te quiero Si me quieres Ahora dimelo\n[02:41.53]En serio tu me gustas\n[02:43.17]mucho mucho mucho mucho\n[02:45.90]Yo te quiero Si me quieres Ahora dimelo\n[02:49.09]Dime hable de mas\n[02:52.32]No puedo pensar\n[02:54.37]En mi mente estas\n[02:57.29]En serio tu me gustas\n[02:58.90]mucho mucho mucho mucho\n[03:01.69]Yo te quiero Si m quieres Ahora dimelo\n[03:05.06]En serio tu me gustas\n[03:06.78]mucho mucho mucho mucho\n[03:09.53]Yo te quiero Si me quieres Ahora dimelo\n[03:13.00]En serio tu me gustas\n[03:14.61]mucho mucho mucho mucho\n[03:17.37]Yo te quiero Si me quieres Ahora dimelo\n',
-      },
-    };
+      audio: [],
+    }
+
   },
+  mounted(){
+		
+		if( parseInt(jQuery(".album_id").val()) > 0){
+
+			var api_url = 'http://mix.mn1.net/api/album/' + jQuery(".album_id").val();
+
+	    axios.get(api_url).then(response => {
+	      for ( var k in response.data){
+	          if(response.data[k]){
+	            var obj = {};
+	            obj.name = response.data[k].filename;
+	            obj.artist = response.data[k].artist;
+	            obj.url = response.data[k].mp3_path;
+	            obj.cover = response.data[k].img_url;
+	            obj.lrc = '[by:' + obj.artist + ']\n[00:00.00] Playtime ' + response.data[k].length_min + ':' + response.data[k].length_sec + ' Frequency ' + response.data[k].frequency + '\n';
+
+	//            if(k==0){
+	//	            this.audio = data[k];
+	//	          }
+	 						
+	            this.audio.push(obj);
+	 						 
+	          }
+	      }
+	    });		
+
+		}
+
+  }
+
 };
 </script>
