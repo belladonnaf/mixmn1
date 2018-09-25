dd2111c6b3f1b52705a78df2f5bb411d6c932645
belladonnaf <belladonnaf@gmail.com>
2018-09-26 01:21:52 +0900

    add stream set file

---
+++ resources/views/favorites/stream-set.blade.php
@@ -0,0 +1,148 @@
+@extends('layouts.backend')
+<!-- Page JS Plugins CSS -->
+
+@section('css_after')
+endsection
+
+@section('content')
+
+<!-- Page Content -->
+<div class="content">
+
+	<div class="row">
+		<div class="col-md-6 col-xl-6">
+
+			<div class="block block-bordered">
+			  <div class="block-header block-header-default">
+			      <h3 class="block-title">Stream Set List</h3>
+			  </div>
+			  <div class="block-content">
+			      <p>Click the headset icon to listen to the music and Click the button to edit the stream set in detail.</p>
+			  </div>
+			</div>
+                            
+			<div class="album-list-wrapper bg-white">
+
+			<div class="js-task-list">
+
+          <!-- Task -->
+@foreach($arr_set as $r)
+          <div class="js-task block block-rounded block-fx-pop block-fx-pop mb-2 animated fadeIn" data-task-id="9" data-task-completed="false" data-task-starred="false">
+              <table class="table table-borderless table-vcenter mb-0">
+                  <tbody><tr>
+                      <td class="text-center pr-0" style="width: 38px;">
+                          <div class="js-task-status custom-control custom-checkbox custom-checkbox-rounded-circle custom-control-primary custom-control-lg">
+                              <input type="checkbox" class="custom-control-input" id="tasks-cb-id9" name="tasks-cb-id9">
+                              <label class="custom-control-label" for="tasks-cb-id9"></label>
+                          </div>
+                      </td>
+                      <td class="js-task-content font-w600 pl-0">
+                      {{$r['set_alias']}}
+                      </td>
+                      <td class="text-right" style="width: 100px;">
+                          <button type="button" class="js-task-star btn btn-sm btn-link text-primary btn-play-music" data-id="{{$r['id']}}">
+                              <i class="far fa-headphones fa-w"></i>
+                          </button>
+                          <button type="button" class="js-task-star btn btn-sm btn-link text-warning btn-open-detail" data-value="{{ json_encode($r['details']) }}">
+                              <i class="far fa-folder-open fa-fw"></i>
+                          </button>
+                          <button type="button" class="js-task-remove btn btn-sm btn-link text-danger" btn-delete-set" data-id="{{$r['id']}}">
+                              <i class="fa fa-times fa-fw"></i>
+                          </button>
+                      </td>
+                  </tr>
+              </tbody></table>
+          </div>
+          <!-- END Task -->
+
+      </div>
+                                
+			</div>
+			<div class="button-wrapper mt-1">
+				<button type="button" class="btn btn-lg btn-warning w-100" id="btn-save-order">Save List Order</button>
+			</div>
+		</div>
+		<div class="col-md-6 col-xl-6">
+
+			<div class="block block-bordered">
+			  <div class="block-header block-header-default">
+			      <h3 class="block-title">Browser</h3>
+			  </div>
+			  <div class="block-content">
+			      <p>You can queue multiple albums and listen in series.</p>
+			  </div>
+			</div>
+
+			<div class="play-list-wrapper bg-white">
+		    <ul class="target">
+
+		    </ul>
+			</div>
+
+			<div class="button-wrapper mt-1">
+				<button type="button" class="btn btn-lg btn-success w-100" id="btn-create-stream-set">Save Stream Set</button>
+			</div>
+
+		</div>
+</div>
+
+<!-- END Page Content -->
+
+@endsection
+@section('js_after')
+<!-- Page JS Plugins -->
+<script src="/js/plugins/bootstrap-notify/bootstrap-notify.min.js"></script>
+
+<!-- Page JS Helpers (BS Notify Plugin) -->
+<script>jQuery(function(){ Dashmix.helpers('notify'); });</script>
+
+<script src="/js/plugins/html5sortable/jquery.sortable.min.js"></script>
+
+<script type="text/javascript">
+	
+	var arr_stream_set = [];
+
+  jQuery(function () {
+    jQuery(".target").sortable({
+    });
+  });
+
+	jQuery(".btn-play-music").click(function(){
+			
+		var set_id = jQuery(this).attr("data-id");
+		window.location.href = '/stream-set/' + set_id;
+		
+	});
+	
+	jQuery(".btn-open-detail").click(function(){
+			
+		var details = jQuery(this).attr("data-value");
+		var obj_details = JSON.parse(details);
+		console.log(obj_details);
+			
+	});
+	
+	jQuery("#btn-save-stream-set").click(function(){
+		
+		jQuery("ul.target li").each(function(){
+			arr_stream_set.push( jQuery(this).attr("data-track-id") );
+		});
+
+		if(arr_stream_set.length < 1){
+			Dashmix.helpers('notify', {type: 'danger', icon: 'fa fa-times mr-1', message: 'At least one of tracks is required.'});
+		}
+
+		var api_url = 'http://mix.mn1.net/api/favorites/save-stream-set';
+		var str_stream_set = JSON.stringify(arr_stream_set); 
+	  axios.post(api_url,{ ids: str_stream_set }).then(response => {
+	
+			if( response.data == 'ok' ){
+				Dashmix.helpers('notify', {type: 'success', icon: 'fa fa-check mr-1', message: 'Stream set saved.'});
+			}
+
+		});
+
+	});
+
+</script>
+@endsection
