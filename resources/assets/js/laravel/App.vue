cbd357370992115e68dd900fe0d216993dd30d95
belladonnaf <belladonnaf@gmail.com>
2018-09-25 09:21:53 +0900

    fixed

--- resources/assets/js/laravel/App.vue
+++ resources/assets/js/laravel/App.vue
@@ -16,34 +16,27 @@ export default {
 
   },
   mounted(){
-		
-		if( parseInt(jQuery(".album_id").val()) > 0){
+		console.log(jQuery(".album_id").val());
+		var api_url = 'http://mix.mn1.net/api/album/' + jQuery(".album_id").val();
+    axios.get(api_url).then(response => {
+      for ( var k in response.data){
+          if(response.data[k]){
+            var obj = {};
+            obj.name = response.data[k].filename;
+            obj.artist = response.data[k].artist;
+            obj.url = response.data[k].mp3_path;
+            obj.cover = response.data[k].img_url;
+            obj.lrc = '[by:' + obj.artist + ']\n[00:00.00] Playtime ' + response.data[k].length_min + ':' + response.data[k].length_sec + ' Frequency ' + response.data[k].frequency + '\n';
 
-			var api_url = 'http://mix.mn1.net/api/album/' + jQuery(".album_id").val();
-
-	    axios.get(api_url).then(response => {
-	      for ( var k in response.data){
-	          if(response.data[k]){
-	            var obj = {};
-	            obj.name = response.data[k].filename;
-	            obj.artist = response.data[k].artist;
-	            obj.url = response.data[k].mp3_path;
-	            obj.cover = response.data[k].img_url;
-	            obj.lrc = '[by:' + obj.artist + ']\n[00:00.00] Playtime ' + response.data[k].length_min + ':' + response.data[k].length_sec + ' Frequency ' + response.data[k].frequency + '\n';
-
-	//            if(k==0){
-	//	            this.audio = data[k];
-	//	          }
-	 						
-	            this.audio.push(obj);
-	 						 
-	          }
-	      }
-	    });		
-
-		}
-
+//            if(k==0){
+//	            this.audio = data[k];
+//	          }
+ 						
+            this.audio.push(obj);
+ 						 
+          }
+      }
+    });		
   }
-
 };
 </script>
