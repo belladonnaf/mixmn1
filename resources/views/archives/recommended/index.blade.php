e78fc89ef2045b0e93fa56d7024253703fee6288
belladonnaf <belladonnaf@gmail.com>
2018-09-26 09:49:03 +0900

    tested

--- resources/views/archives/recommended/index.blade.php
+++ resources/views/archives/recommended/index.blade.php
@@ -18,94 +18,10 @@ endsection
 			        <p class="text-white text-uppercase font-size-sm font-w700 text-center mt-2 mb-4">
 			            1 - 10 RANK
 			        </p>
+<?php
 
-@foreach ($first_arr as $k=>$row)
-<?php var_dump($row); ?>
-			        <a class="block block-rounded block-link-rotate bg-black-10 mb-2" href="javascript:void(0)">
-			            <div class="block-content block-content-sm block-content-full d-flex align-items-center justify-content-between">
-			                <div class="item">
-  {{--
-	@if( $row['image'] > '' )
-													<img src="{{$row['image']}}" width="64px" height="64px">
-	@else
-			                    <i class="fa fa-2x fa-film text-primary-lighter"></i>
-	@endif			                
-  --}}			                
-			                </div>
-			                <div class="mr-3">
-			                    <p class="text-white font-size-h3 font-w300 mb-0">
-			                       <span class="rank rank-{{($k+1)}}">{{($k+1)}}</span> <span class="album-info">{{$r['album_info]}}</span>
-			                    </p>
-			                </div>
-			            </div>
-			        </a>
-@endforeach
-			    </div>
-			</div>
-
-		</div>
-
-		<div class="col-md-6 col-xl-4">
-	
-			<div class="block block-rounded bg-gd-primary">
-			    <div class="block-content">
-			        <p class="text-white text-uppercase font-size-sm font-w700 text-center mt-2 mb-4">
-			            11 - 20 RANK
-			        </p>
-
-@foreach ($second_arr as $k=>$row)
-			        <a class="block block-rounded block-link-rotate bg-black-10 mb-2" href="javascript:void(0)">
-			            <div class="block-content block-content-sm block-content-full d-flex align-items-center justify-content-between">
-			                <div class="item">
-  {{--
-  @if( $row['image'] > '' )
-													<img src="{{$row['image']}}" width="64px" height="64px">
-	@else
-			                    <i class="fa fa-2x fa-film text-primary-lighter"></i>
-	@endif			                
-  --}}			                
-			                </div>
-			                <div class="mr-3">
-			                    <p class="text-white font-size-h3 font-w300 mb-0">
-			                       <span class="rank rank-{{($k+11)}}">{{($k+11)}}</span> <span class="album-info">{{$r['album_info]}}</span>
-			                    </p>
-			                </div>
-			            </div>
-			        </a>
-@endforeach
-			    </div>
-			</div>
-
-		</div>
-
-		<div class="col-md-6 col-xl-4">
-	
-			<div class="block block-rounded bg-gd-primary">
-			    <div class="block-content">
-			        <p class="text-white text-uppercase font-size-sm font-w700 text-center mt-2 mb-4">
-			           21 - 30 RANK
-			        </p>
-
-@foreach ($third_arr as $k=>$row)
-			        <a class="block block-rounded block-link-rotate bg-black-10 mb-2" href="javascript:void(0)">
-			            <div class="block-content block-content-sm block-content-full d-flex align-items-center justify-content-between">
-			                <div class="item">
-  {{--
-	@if( $row['image'] > '' )
-													<img src="{{$row['image']}}" width="64px" height="64px">
-	@else
-			                    <i class="fa fa-2x fa-film text-primary-lighter"></i>
-	@endif			                
-  --}}			                
-			                </div>
-			                <div class="mr-3">
-			                    <p class="text-white font-size-h3 font-w300 mb-0">
-			                       <span class="rank rank-{{($k+21)}}">{{($k+21)}}</span> <span class="album-info">{{$r['album_info]}}</span>
-			                    </p>
-			                </div>
-			            </div>
-			        </a>
-@endforeach
+var_dump($first_arr);
+?>
 			    </div>
 			</div>
 
