3b69a40f7f706879b8b8aac3f6e034d2b3fe7c2f
belladonnaf <belladonnaf@gmail.com>
2018-09-27 23:10:15 +0900

    adding apk installation

---
+++ resources/views/pages/apkinstallation.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.backend')
+<!-- Page JS Plugins CSS -->
+@section('content')
+    <!-- Page Content -->
+    <div class="content">
+        <!-- Your Block -->
+        <div class="block block-rounded block-bordered">
+            <div class="block-header block-header-default">
+                <h3 class="block-title">About APK installation</h3>
+            </div>
+            <div class="block-content">
+                <p><a href="/apk/MIXMn1.apk">Click This Link</a></p>
+            </div>
+        </div>
+        <!-- END Your Block -->
+    </div>
+    <!-- END Page Content -->
+@endsection
